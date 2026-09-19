cbd0aae6502bd7941f931a93ce214aa6ea79a6f6
Dante Vazquez <dantesurf9@gmail.com>
2026-09-19 13:32:07 -0600

    changes

--- xdg-desktop-portal-termfilechooser/yazi-wrapper.sh
+++ xdg-desktop-portal-termfilechooser/yazi-wrapper.sh
@@ -29,26 +29,9 @@ fi
 COLUMNS=110
 LINES=30
 
-# Approximate center coordinates for immediate effect
-POS_ARGS=""
-res=$(xdpyinfo 2>/dev/null | awk '/dimensions:/ {print $2}')
-if [ -n "$res" ]; then
-    screen_w="${res%x*}"
-    screen_h="${res#*x}"
-    win_w=$(( COLUMNS * 24 ))
-    win_h=$(( LINES * 53 ))
-    pos_x=$(( (screen_w - win_w) / 2 ))
-    pos_y=$(( (screen_h - win_h) / 2 ))
-    if [ "$pos_x" -gt 0 ] && [ "$pos_y" -gt 0 ]; then
-        POS_ARGS="-o window.position.x=$pos_x -o window.position.y=$pos_y"
-    fi
-fi
-
-# Launch Alacritty with class yazi-chooser and dimensions
-alacritty --class yazi-chooser,yazi-chooser -T "File Chooser" \
-    -o window.dimensions.columns=$COLUMNS \
-    -o window.dimensions.lines=$LINES \
-    $POS_ARGS \
+# Launch foot with app ID yazi-chooser and dimensions
+foot --app-id=yazi-chooser -T "File Chooser" \
+    --window-size-chars="${COLUMNS}x${LINES}" \
     -e "$cmd" "$@"
 
 if [ "$directory" = "1" ]; then
